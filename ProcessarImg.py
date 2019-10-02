6e94b1e73d2b4a7c17b4ce4d8d18d3a4316024cf
Alfredo Albélis <alfredoalbelisb@gmail.com>
2019-10-02 11:53:15 -0300

    crop RA and rectAnswers

--- ProcessarImg.py
+++ ProcessarImg.py
@@ -1,12 +1,35 @@
 import cv2
-import numpy as np
 
-img = cv2.imread('ModeloGabarito\VetorialGabarito.png')
-edged = cv2.Canny(img, 30, 200) 
+imageOriginal = cv2.imread('ModeloGabarito\VetorialGabarito.png')
+resized_image = cv2.resize(imageOriginal, (800, 800))
 
-contours, hierarchy = cv2.findContours(edged, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+xCoordDict = {
+'01-A_E' : [83, 121, 159, 196, 234],
+'11-A_E' : [329, 367, 405, 442, 480],
+'21-A_E' : [575, 613, 651, 688, 725]
+}
 
-cv2.drawContours(img, contours, -1, (0, 255, 0), 1) 
-cv2.imshow("Keypoints", img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+#for key in xCoordDict:
+#    
+#    yCoordRect = 207
+#    xListCoord = xCoordDict[key]
+#    
+#    for i in range(10):
+#        for xCoordRect in xListCoord:
+#            
+#            crop = resized_image[yCoordRect:yCoordRect + 9, xCoordRect:xCoordRect + 30]
+#            cv2.imshow('Image', crop)
+#            cv2.waitKey(0)
+#            
+#        yCoordRect = yCoordRect + 20 #Next line
+
+RA = [294, 333, 371, 409, 447, 485]
+yCoordRA = 62
+height = 31
+width = 29
+
+#for xCoordRA in RA:
+#    
+#    crop = resized_image[yCoordRA: yCoordRA + height, xCoordRA:xCoordRA + width]
+#    cv2.imshow('Image', crop)
+#    cv2.waitKey(0)
