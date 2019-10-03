29bc6a2bc82cba8b1ffec613f26084e7ada6b1bc
Alfredo Albélis <alfredoalbelisb@gmail.com>
2019-10-03 10:57:41 -0300

    Add function mask()

--- AutoCorrection.py
+++ AutoCorrection.py
@@ -1,16 +1,25 @@
 import cv2
+import numpy as np
+from copy import deepcopy
 
 imageOriginal = cv2.imread('ModeloGabarito\GabaritoTeste.png')
 resized_image = cv2.resize(imageOriginal, (800, 800))
+maskImage = np.zeros(resized_image.shape,np.uint8)
+maskAux = deepcopy(maskImage)
 
 def cropImage(xCoord, yCoord, height, width):
-    
-    crop = resized_image[yCoord:yCoord + height, xCoord:xCoord + width]
+    global resized_image
+    crop = resized_image[yCoord:yCoord + height, xCoord:xCoord + width]    
     cv2.imshow('Image', crop)
     cv2.waitKey(0)
 
+def mask(xCoord, yCoord, height, width):
+    global maskImage
+    maskImage[yCoord:yCoord + height, xCoord:xCoord + width] = resized_image[yCoord:yCoord + height, xCoord:xCoord + width]
+    
 def multipleChoice():
     
+    global maskImage, maskAux    
     xVertexDict = {
         '01_Vertex_ABCDE' : [83, 121, 159, 196, 234],
         '11_Vertex_ABCDE' : [329, 367, 405, 442, 480],
@@ -25,23 +34,31 @@ def multipleChoice():
         xListCoord = xVertexDict[key]
         
         for i in range(10): # 10 questions
+            maskImage = deepcopy(maskAux)
             for xCoordRect in xListCoord:
-                cropImage(xCoordRect, yCoordRect, heightRect, widthRect)
-                
+                mask(xCoordRect, yCoordRect, heightRect, widthRect)
+            
+            cv2.imshow('Image', maskImage)
+            cv2.waitKey(0)
             yCoordRect = yCoordRect + 20 #Next Question
 
 def RA():
-
+    
+    global maskImage, maskAux
     xVertexRA = [294, 333, 371, 409, 447, 485]
     yCoordRA = 62
     heightSquare = 31
     widthSquare = 29
     
     for xCoordRA in xVertexRA:
-        cropImage(xCoordRA, yCoordRA, heightSquare, widthSquare)
-        
+        mask(xCoordRA, yCoordRA, heightSquare, widthSquare)
+    
+    cv2.imshow('Image', maskImage)
+    cv2.waitKey(0)
+
 def otherAnswers():
     
+    global maskImage, maskAux
     xVertexDict = {
         '31_Vertex_ABCDE' : [129, 167, 205, 243, 280, 318],
         '36_Vertex_ABCDE' : [469, 507, 544, 582, 620, 658]
@@ -55,11 +72,14 @@ def otherAnswers():
         xListCoord = xVertexDict[key]
         
         for i in range(5): # 5 Questions
+            maskImage = deepcopy(maskAux)
             for xCoord in xListCoord:
-                cropImage(xCoord, yCoord, heightSquare, widthSquare)
-                
+                mask(xCoord, yCoord, heightSquare, widthSquare)
+
+            cv2.imshow('Image', maskImage)
+            cv2.waitKey(0)                
             yCoord = yCoord + 44 #Next Question
-        
-#multipleChoice()
-#RA()
-#otherAnswers()
+
+multipleChoice()
+RA()
+otherAnswers()
