d7f79dfa909af6e72f1583fee15fc31e67e442ba
Alfredo Albélis <alfredoalbelisb@gmail.com>
2019-10-31 00:00:29 -0300

    add func testModels

--- TrainModels.py
+++ TrainModels.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from keras.callbacks import EarlyStopping, ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+from tensorflow.python.util import deprecation
 from keras.layers.convolutional import MaxPooling2D
 from keras.layers.convolutional import Conv2D
 from keras.layers import GlobalMaxPooling2D
@@ -15,12 +16,10 @@ import requests
 import cv2
 import os
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' #INFO, WARNING, and ERROR messages are not printed
-
 def download_file(url):
     local_filename = url.split('/')[-1]
     # NOTE the stream=True parameter below
-    with requests.get(url, stream=True) as r:
+    with requests.get(url, stream = True) as r:
         r.raise_for_status()
         with open(local_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=8192): 
@@ -66,9 +65,8 @@ def load_data(path):
         x_train, y_train = f['x_train'], f['y_train']
         x_test, y_test = f['x_test'], f['y_test']
 
-        x_train = x_train.reshape(x_train.shape[0], 1, 28, 28).astype('float32')
-
-        x_test = x_test.reshape(x_test.shape[0], 1, 28, 28).astype('float32')
+        x_train = x_train.reshape(x_train.shape[0], 28, 28, 1).astype('float32')
+        x_test = x_test.reshape(x_test.shape[0], 28, 28, 1).astype('float32')
 
         x_train = x_train / 255
         x_test = x_test / 255
@@ -78,70 +76,36 @@ def load_data(path):
 
     return x_train, y_train, x_test, y_test
 
-def tests(model):
+downloadFiles()
+np.random.seed(2019)
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+x_train, y_train, x_test, y_test = load_data('files/MnistKeras.npz')
 
-    images = ['files/number-one.png', 'files/number-two.png', 'files/number-three.png', 'files/number-four.png', 'files/number-five.png',
-              'files/number-six.png', 'files/number-seven.png', 'files/number-eight.png', 'files/number-nine.png'
-              ]
+class Model:
+    def __init__(self):
+        self.model = self.getModel()
 
-    for image in images:
-        img_pred = cv2.imread(image, 0)
-        plt.imshow(img_pred, cmap='gray')
-    
-        if img_pred.shape != [28,28]:
-            img2 = cv2.resize(img_pred, (28, 28))
-            img_pred = img2.reshape(28, 28, -1)
-        else:
-            img_pred = img_pred.reshape(28, 28, -1)
-    
-        img_pred = ~img_pred
-        img_pred = img_pred.reshape(1, 1, 28, 28).astype('float32')
-    
-        img_pred = img_pred/255.0
-    
-    
-        pred = model.predict_classes(img_pred)
-        pred_proba = model.predict_proba(img_pred)
-        pred_proba = "%.2f%%" % (pred_proba[0][pred]*100)
-        print(pred[0], " com confiança de ", pred_proba)
-        plt.show()
-
-class LoadModel:
-    def __init__(self, name):
-        self.name = name
-        self.model = Model()
-        self.model.load_weights(name)
-
-    def printAcc(self):
-        scores = self.model.evaluate(x_test, y_test, verbose=0)
-        print("\nAcc: %.2f%%" % (scores[1]*100))
-    
-    def testModel(self):
-        tests(self.model)
-
-class CreateModel:
-    def __init__(self, name, epochs, batch_size, nameCallback, patienceCallback):
+    def defineTrain(self, name, epochs, batch_size, nameCallback, patienceCallback):
         self.name = name
         self.epochs = epochs
         self.batch_size = batch_size
         self.nameCallback = nameCallback
         self.patienceCallback = patienceCallback
-        self.model = Model()
-    
+        
     def get_callbacks(self, name, patience_lr):
-        mcp_save = ModelCheckpoint(name+".h5", save_best_only=True,
-                                         monitor='val_accuracy', mode='max')
-        reduce_lr_loss = ReduceLROnPlateau(monitor='loss', factor=0.1,
-                                           patience=patience_lr,
-                                           verbose=1, epsilon=1e-4,
-                                           mode='min')
-        early_stop = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10,
-                                   verbose=0, mode='max', baseline=None)
-        csv_logger = CSVLogger(name + '_log.csv', append=True, separator=';')
+        mcp_save = ModelCheckpoint(name + '.h5', save_best_only = True,
+                                         monitor = 'val_accuracy', mode='max')
+        reduce_lr_loss = ReduceLROnPlateau(monitor = 'loss', factor = 0.1,
+                                           patience = patience_lr,
+                                           verbose = 1, min_delta = 1e-4,
+                                           mode = 'min')
+        early_stop = EarlyStopping(monitor = 'val_accuracy', min_delta = 0, patience = 10,
+                                   verbose = 0, mode = 'max', baseline = None)
+        csv_logger = CSVLogger(name + '_log.csv', append = True, separator = ';')
         return [reduce_lr_loss, early_stop, csv_logger, mcp_save]
     
-    def testModel(self):
-        tests(self.model)
+    def loadModel(self, nameLoad):
+        self.model.load_weights(nameLoad)
         
     def trainModel(self):
         self.model.summary()
@@ -152,45 +116,66 @@ class CreateModel:
         
         self.model.save_weights(self.name)
         
-        scores = self.model.evaluate(x_test, y_test, verbose=0)
+    def printAcc(self):
+        scores = self.model.evaluate(x_test, y_test, verbose = 0)
         print("\nAcc: %.2f%%" % (scores[1]*100))
+    
+    def testModel(self):
+        images = ['files/number-one.png', 'files/number-two.png', 'files/number-three.png', 'files/number-four.png', 'files/number-five.png',
+              'files/number-six.png', 'files/number-seven.png', 'files/number-eight.png', 'files/number-nine.png'
+              ]
+
+        for image in images:
+            img_pred = cv2.imread(image, 0)
+            plt.imshow(img_pred, cmap='gray')
+    
+            if img_pred.shape != [28,28]:
+                img2 = cv2.resize(img_pred, (28, 28))
+                img_pred = img2.reshape(28, 28, -1)
+            else:
+                img_pred = img_pred.reshape(28, 28, -1)
+    
+            img_pred = ~img_pred
+            img_pred = img_pred.reshape(1, 28, 28, 1).astype('float32')
+
+            img_pred = img_pred/255.0
+    
+            pred = self.model.predict_classes(img_pred)
+            pred_proba = self.model.predict_proba(img_pred)
+            pred_proba = "%.2f%%" % (pred_proba[0][pred]*100)
+            print(pred[0], " com confiança de ", pred_proba)
+            plt.show()
         
-def Model():
-    num_classes = 10
-    model = Sequential()
+    def getModel(self):
 
-    model.add(Conv2D(128, (5, 5), input_shape=(1, 28, 28), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.2))
+        num_classes = 10
+        model = Sequential()
+
+        model.add(Conv2D(128, (5, 5), input_shape = (28, 28, 1), activation = 'relu'))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.2))
+
+        model.add(Conv2D(256, (3, 3), input_shape=(1, 28, 28), activation = 'relu'))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.2))
     
-    model.add(Conv2D(256, (3, 3), input_shape=(1, 28, 28), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.2))
+        model.add(Conv2D(512, (3, 3), input_shape=(1, 28, 28), activation = 'relu'))
+        model.add(MaxPooling2D(pool_size = (2, 2)))
+        model.add(Dropout(0.3))
+            
+        model.add(GlobalMaxPooling2D())
+        
+        model.add(Dense(1024, activation='relu'))
+        model.add(Dropout(0.5))
+        model.add(Dense(num_classes, activation='softmax', name='predict'))
     
-    model.add(Conv2D(512, (3, 3), input_shape=(1, 28, 28), activation = 'relu'))
-    model.add(MaxPooling2D(pool_size = (2, 2)))
-    model.add(Dropout(0.3))
-
-    model.add(GlobalMaxPooling2D())
+        model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
     
-    model.add(Dense(1024, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(num_classes, activation='softmax', name='predict'))
+        return model
 
-    model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
-
-    return model
-
-np.random.seed(2019)
-downloadFiles()
-x_train, y_train, x_test, y_test = load_data('files/MnistKeras.npz')
-
-#create and train model
-#brain = CreateModel(name = 'Brain.h5', epochs = 1000, batch_size = 32, nameCallback = 'Albelis', patienceCallback = 10)
+brain = Model()
+brain.loadModel('files/Brain_SGD_1024.h5')
+#brain.defineTrain(name = 'Brain.h5', epochs = 1000, batch_size = 32, nameCallback = 'BrainCallback', patienceCallback = 10)
 #brain.trainModel()
-#brain.testModel()
-
-#load model and tests
-brainLoad = LoadModel(name = 'files/Brain_SGD_1024.h5')
-brainLoad.testModel()
-#brainLoad.printAcc()
+brain.testModel()
+brain.printAcc()
